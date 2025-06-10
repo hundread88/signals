1dd93a4ba8da46080ea51ae4cd104b2b9e7177b5
hundread88 <166928534+hundread88@users.noreply.github.com>
2025-06-11 03:06:08 +0400

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,14 +1,26 @@
-
 import { Low } from 'lowdb';
 import { JSONFile } from 'lowdb/node';
-import { join } from 'path';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
 
-const file = join('data', 'db.json');
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const file = join(__dirname, '../data/db.json');
+
+// Убедимся, что папка и файл существуют
+if (!fs.existsSync(join(__dirname, '../data'))) {
+  fs.mkdirSync(join(__dirname, '../data'), { recursive: true });
+}
+if (!fs.existsSync(file)) {
+  fs.writeFileSync(file, JSON.stringify({ users: [] }, null, 2));
+}
+
 const adapter = new JSONFile(file);
 const db = new Low(adapter);
 
 await db.read();
 db.data ||= { users: [] };
+await db.write();
 
 export async function getUser() {
   await db.read();

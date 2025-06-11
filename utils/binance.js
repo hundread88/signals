1442eeaf208d42ffbe90f57971f6117c2e3f3d67
hundread88 <166928534+hundread88@users.noreply.github.com>
2025-06-11 18:43:36 +0400

    Update binance.js

--- utils/binance.js
+++ utils/binance.js
@@ -1,8 +1,7 @@
-
 import axios from 'axios';
 
-export async function getCandles(symbol, interval) {
-  const url = `https://api.binance.com/api/v3/klines?symbol=${symbol}&interval=${interval}&limit=100`;
+export async function getCandles(symbol, interval, limit = 200) {
+  const url = `https://api.binance.com/api/v3/klines?symbol=${symbol}&interval=${interval}&limit=${limit}`;
   const response = await axios.get(url);
   return response.data;
 }

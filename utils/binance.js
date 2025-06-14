f3c0260cd5fe96afe599ea4021b839e4f466e7b8
hundread88 <166928534+hundread88@users.noreply.github.com>
2025-06-14 07:11:24 +0400

    Update binance.js

--- utils/binance.js
+++ utils/binance.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
 
+// Эта функция у вас уже есть
 export async function getCandles(symbol, interval, limit = 200) {
   const url = `https://api.binance.com/api/v3/klines?symbol=${symbol}&interval=${interval}&limit=${limit}`;
   const response = await axios.get(url);
   return response.data;
+}
+
+// ЭТУ ФУНКЦИЮ НУЖНО ДОБАВИТЬ
+export async function getCurrentPrice(symbol) {
+  const url = `https://api.binance.com/api/v3/ticker/price?symbol=${symbol}`;
+  const response = await axios.get(url);
+  return parseFloat(response.data.price);
 }
